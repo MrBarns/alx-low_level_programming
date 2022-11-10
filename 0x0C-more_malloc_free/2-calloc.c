95d5985faabab036c7b454ac9ed9aa617e216211
MrBarns <barnabasoluwatosin@gmail.com>
2022-11-10 18:49:14 +0100

    Debug attempt 4

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -19,7 +19,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	arr = malloc(size * nmemb);
+	arr = malloc(nmemb * size);
 
 	if (arr == NULL)
 	{
