eccf63915f74e5d1b20789e960e12cdfa2a98bd0
MrBarns <barnabasoluwatosin@gmail.com>
2022-11-28 19:31:37 +0100

    Add Task 0 script

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * print_list -  prints all the elements of a list_t list
+ * @h: pointer to a list_t list
+ *
+ * Return: number of nodes in list_t list
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t nodeNums = 1;
+	list_t *next = h->next;
+
+	if (h->str == NULL)
+		printf("[0] (nil)\n");
+	else
+		printf("%s\n", h->str);
+
+	if (next)
+		nodeNums += print_list(next);
+
+	return (nodeNums);
+}
