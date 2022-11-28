37ce463184018ee39771ac04ee907a256ef7c892
MrBarns <barnabasoluwatosin@gmail.com>
2022-11-28 19:49:49 +0100

    Debug Task0 attempt 2

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,16 +11,20 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t nodeNums = 1;
+	size_t nodeNums = 0;
 	list_t *next = h->next;
 
-	if (h->str == NULL)
-		printf("[0] (nil)\n");
-	else
-		printf("[%d] %s\n", h->len, h->str);
+	if (h != NULL)
+	{
+		nodeNums = 1;
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
 
-	if (next)
+		if (next)
 		nodeNums += print_list(next);
+	}
 
 	return (nodeNums);
 }
