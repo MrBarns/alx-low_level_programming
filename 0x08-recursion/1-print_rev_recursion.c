6a0bc1a19dcba4d948218b3e9b88d59a12f10412
MrBarns <barnabasoluwatosin@gmail.com>
2023-02-15 14:32:21 +0100

    Add task1 script

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -11,11 +11,8 @@
 void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
-	{
-		_putchar(*(s - 1));
 		return;
-	}
 
-	_putchar(_print_rev_recursion(s + 1));
-	return (*(s - 1));
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
 }
