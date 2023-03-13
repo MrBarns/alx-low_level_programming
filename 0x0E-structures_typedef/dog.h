6e21c39146499f9ab3eeebf782519b20edf9c54a
MrBarns <barnabasoluwatosin@gmail.com>
2023-03-13 14:26:05 +0100

    Add task0 script

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - struct object representing a dog
+ *
+ * @name: name of dog
+ * @age: age of dog in years
+ * @owner: name of dog owner
+ *
+ */
+
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
