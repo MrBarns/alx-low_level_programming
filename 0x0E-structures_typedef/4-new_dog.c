ddb8709d09cf8d2aa65eb04414e24d980addff83
MrBarns <barnabasoluwatosin@gmail.com>
2023-03-14 08:50:54 +0100

    Debug task4 script

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -17,8 +17,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 	unsigned int n, o, c;
 	char *new_n, *new_o;
 
-	doggy = malloc(sizeof(dog_t));
+	if (name == NULL || owner == NULL)
+		return (NULL);
 
+	doggy = malloc(sizeof(dog_t));
 	if (doggy == NULL)
 	{
 		free(doggy);
