caee7e4ae538372a910bdb9d839a4fc5f41747c8
MrBarns <barnabasoluwatosin@gmail.com>
2023-03-16 17:42:08 +0100

    Add task0 script

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - returns the sum of all its parameters.
+ * @n: number of parameters entered
+ *
+ * Return: sum of parameters if successful
+ * 0 otherwise
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	int sum;
+	unsigned int count;
+
+	if (n == 0)
+		return (0);
+
+	va_start(ap, n);
+
+	sum = 0;
+
+	for (count = 0; count < n; count++)
+		sum += va_arg(ap, int);
+
+	va_end(ap);
+
+	return (sum);
+}
