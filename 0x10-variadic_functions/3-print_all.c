1d328b8520bf903aa2f27159a1a8132c75ec8b73
MrBarns <barnabasoluwatosin@gmail.com>
2023-03-22 15:46:29 +0100

    Debug task3 script

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -12,6 +12,7 @@ void print_all(const char * const format, ...)
 	va_list vlist;
 	unsigned int n = 0, c = 0;
 	int tis;
+	char *str;
 
 	while (*(format + n) != '\0')
 		n++;
@@ -33,7 +34,14 @@ void print_all(const char * const format, ...)
 			printf("%d", va_arg(vlist, int));
 			break;
 		case 's':
-			printf("%s", va_arg(vlist, char *));
+			str = va_arg(vlist, char *);
+
+			if (!str)
+			{
+				printf("(nil)");
+				break;
+			}
+			printf("%s", str);
 			break;
 		default:
 			tis = 0;
