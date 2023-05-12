5151f6afbc86ae5b7978ac56da03a83a9731b4e6
MrBarns <barnabasoluwatosin@gmail.com>
2023-05-12 18:24:07 +0100

    Make script5 betty compliant

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -2,8 +2,8 @@
 
 
 /**
- * flip_bits - returns the number of bits you would need to flip to get from one
- * number to another
+ * flip_bits - returns the number of bits you would need to flip to get from
+ * one number to another
  *
  * @n: starting value
  * @m: ending value
