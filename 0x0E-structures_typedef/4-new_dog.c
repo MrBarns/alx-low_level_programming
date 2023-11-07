d652a18e223522d8ce9f157552541781e68102e5
MrBarns <barnabasoluwatosin@gmail.com>
2023-11-07 10:53:39 +0100

    Modify script 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -21,14 +21,23 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 
 	n_dog = malloc(sizeof(dog_t));
-	n_name = malloc(sizeof(name));
-	n_owner = malloc(sizeof(owner));
-
-	if (n_dog == NULL || n_name == NULL || n_owner == NULL)
+	if (n_dog == NULL)
 	{
 		free(n_dog);
+		return (NULL);
+	}
+	for (x = 0; name[x] != '\0'; x++)
+		;
+	n_name = malloc(sizeof(char) * (x + 1));
+	for (x = 0; owner[x] != '\0'; x++)
+		;
+	n_owner = malloc(sizeof(char) * (x + 1));
+
+	if (n_name == NULL || n_owner == NULL)
+	{
 		free(n_name);
 		free(n_owner);
+		free(n_dog);
 		return (NULL);
 	}
 
