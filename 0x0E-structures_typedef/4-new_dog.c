d06c9d047833e27d39bb0b381fea27d9b4c13841
MrBarns <barnabasoluwatosin@gmail.com>
2023-11-07 02:47:44 +0100

    Add last 3 scripts

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * new_dog - creates a new dog
+ *
+ * @name: name of new dog
+ * @age: age of new dog
+ * @owner: new dog's owner
+ *
+ * Return: pointer to new dog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *n_dog;
+	char *n_name, *n_owner;
+	unsigned int x;
+
+	if (name == NULL || owner == NULL)
+		return (0);
+
+	n_dog = malloc(sizeof(dog_t));
+	n_name = malloc(sizeof(name));
+	n_owner = malloc(sizeof(owner));
+
+	if (n_dog == NULL || n_name == NULL || n_owner == NULL)
+	{
+		free(n_dog);
+		free(n_name);
+		free(n_owner);
+		return (0);
+	}
+
+	for (x = 0; name[x] != '\0'; x++)
+		n_name[x] = name[x];
+	for (x = 0; owner[x] != '\0'; x++)
+		n_owner[x] = owner[x];
+
+	n_dog->name = n_name;
+	n_dog->age = age;
+	n_dog->owner = n_owner;
+
+	return (n_dog);
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,7 +8,8 @@
  * @age: age of dog
  * @owner: name of dog's human
  *
- * Description: General representation of a dog for the purpose of alxSE program
+ * Description: General representation of a dog for the purpose of alxSE
+ * program
  */
 
 struct dog
@@ -18,8 +19,14 @@ struct dog
 	char *owner;
 };
 
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
 void print_dog(struct dog *d);
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
 
 #endif
