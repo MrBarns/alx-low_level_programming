8072300903e31767868673b5eb51b1529f8dfc9d
MrBarns <barnabasoluwatosin@gmail.com>
2023-11-17 12:16:15 +0100

    add task3 script

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,51 @@
+#include "lists.h"
+
+
+/**
+ * add_node_end - adds a new node at the end of a list_t list.
+ *
+ * @head: address of pointer to head node.
+ * @str: string to store in new node.
+ *
+ * Return: address of new element on success, NULL otherwise.
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node;
+	char *new_str;
+	unsigned int len;
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_str = strdup(str);
+	if (new_str == NULL)
+	{
+		free(new_str);
+		free(new_node);
+		return (NULL);
+	}
+
+	for (len = 0; new_str[len] != '\0'; len++)
+		;
+
+	new_node->str = new_str;
+	new_node->len = len;
+	new_node->next = NULL;
+
+	if (!(*head))
+		*head = new_node;
+	else
+	{
+		for (; (*head)->next != NULL; head = &((*head)->next))
+			;
+		(*head)->next = new_node;
+	}
+
+	return (new_node);
+}
