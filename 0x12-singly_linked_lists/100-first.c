988fe24e5cebea44be6e0ca78b74d836a523d68d
MrBarns <barnabasoluwatosin@gmail.com>
2023-11-17 15:06:51 +0100

    debug1 task100 script

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -10,8 +10,8 @@ int print_first(void) __attribute__ ((constructor));
 
 int print_first(void)
 {
-	printf("You're beat! and yet, you must allow,"
-"\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 
 	return (0);
 }
