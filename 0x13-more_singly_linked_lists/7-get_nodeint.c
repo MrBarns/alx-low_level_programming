e96863bf41e960db816b1789deb4896d02707b6b
MrBarns <barnabasoluwatosin@gmail.com>
2023-11-20 22:53:22 +0100

    Add task7 script

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,5 +12,15 @@
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	
+	unsigned int i;
+
+	for (i = 0; i < index; i++)
+	{
+		if (!head)
+			return (NULL);
+		head = head->next;
+	}
+	if (head)
+		return (head);
+	return (NULL);
 }
