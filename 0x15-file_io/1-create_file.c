6abb6d4605f637c0ab36b8a2c667ba2a1dd1bd4d
MrBarns <barnabasoluwatosin@gmail.com>
2023-11-28 03:56:22 +0100

    Add task1 script

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+
+/**
+ * create_file - creates a file
+ *
+ * @filename: name of the file to be created
+ * @text_content: NULL terminated string to write to the file
+ *
+ * Return: 1 on success. -1 otherwise.
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t res;
+	size_t len;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd < 0)
+		return (-1);
+
+	for (len = 0; text_content[len]; len++)
+		;
+	if (len)
+	{
+		res = write(fd, text_content, len);
+		close(fd);
+		if (res < 0)
+			return (-1);
+	}
+
+	return (1);
+}
