42823b5113e68fdeddca1849c0755464070d8db0
MrBarns <barnabasoluwatosin@gmail.com>
2023-11-28 03:59:52 +0100

    Debug1 task1 script

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd;
 	ssize_t res;
-	size_t len;
+	size_t len = 0;
 
 	if (!filename)
 		return (-1);
@@ -23,8 +23,11 @@ int create_file(const char *filename, char *text_content)
 	if (fd < 0)
 		return (-1);
 
-	for (len = 0; text_content[len]; len++)
-		;
+	if (text_content)
+	{
+		for (; text_content[len]; len++)
+			;
+	}
 	if (len)
 	{
 		res = write(fd, text_content, len);
